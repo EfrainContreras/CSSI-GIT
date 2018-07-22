c385826ba7c0eafc0dfce6044a5f27a220ea876c
Efrain <demouser@MacBook-Pro.local>
2018-07-21 17:08:32 -0700

    Added some login in code

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ jinja_environment = jinja2.Environment(
     loader = jinja2.FileSystemLoader(
         os.path.dirname(__file__) + '/templates'))
 
-
-
 # TO-DO:
 # 1. Add a way to login and keep track of diffrent users
 # 2. Request input from users
@@ -22,13 +20,31 @@ jinja_environment = jinja2.Environment(
 # 5. Messaging between users
 
 
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
+
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+        else:
+            login_url = users.create_login_url('/')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+
+
+        if user:
+            get_log_inout_url = users.create_logout_url('/')
+        else:
+            get_log_inout_url = users.create_login_url('/')
+
+        log_url = get_log_inout_url
+        variables = {"log_url": log_url}
+
+
         template = jinja_environment.get_template("main.html")
-        self.response.write(template.render())
-
-
+        self.response.write(template.render(variables))
 
 
 
